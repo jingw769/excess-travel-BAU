05bb07f592effbb70adce028249bd3558a2ba8b2
jingw769 <jennywei769@gmail.com>
2024-12-04 09:49:21 +1100

    Add files via upload

---
+++ story_content/user.js
@@ -0,0 +1,37 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "61rMGSVTw53":
+        Script1();
+        break;
+      case "67qJ714zJQp":
+        Script2();
+        break;
+      case "624EV5Yzrul":
+        Script3();
+        break;
+  }
+}
+
+function Script1()
+{
+  document.body.style.backgroundImage = "url('https://www.ses.vic.gov.au/documents/8655930/16022362/Kronos+background-01.png')";
+document.body.style.backgroundSize = "cover";
+document.body.style.backgroundRepeat = "no-repeat";
+}
+
+function Script2()
+{
+  document.body.style.backgroundImage = "url('https://www.ses.vic.gov.au/documents/8655930/16022362/Kronos+background-01.png')";
+document.body.style.backgroundSize = "cover";
+document.body.style.backgroundRepeat = "no-repeat";
+}
+
+function Script3()
+{
+  document.body.style.backgroundImage = "url('https://www.ses.vic.gov.au/documents/8655930/16022362/Kronos+background-01.png')";
+document.body.style.backgroundSize = "cover";
+document.body.style.backgroundRepeat = "no-repeat";
+}
+
